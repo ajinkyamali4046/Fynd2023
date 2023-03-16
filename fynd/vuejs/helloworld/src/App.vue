1357224a74c2becabc14c35e2e1ede150db35519
ajinkyamali4046 <94428953+ajinkyamali4046@users.noreply.github.com>
2023-03-16 20:42:19 +0530

    App

---
+++ fynd/vuejs/helloworld/src/App.vue
@@ -0,0 +1,53 @@
+// <template>
+//   <img alt="Vue logo" src="./assets/logo.png">
+//   <HelloWorld msg="Welcome to Your Vue.js App"/>
+// </template>
+
+// <script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+// </script>
+
+<!-- // <style>
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+// </style> -->
+
+
+<script setup>
+import {RouterLink, RouterView} from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import eg1 from './components/eg1.vue'
+</script>
+
+<template>
+ <header>
+ <div class="wrapper">
+ 
+  <br>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/eg1">Example</RouterLink>
+  </nav>
+  <br>
+
+ </div>
+
+ </header>
+
+ <RouterView/>
+
+</template>
+
