873baa2937524cbc8f3a54e810ba31bc744570a5
ajinkyamali4046 <94428953+ajinkyamali4046@users.noreply.github.com>
2023-03-30 21:01:45 +0530

    Test 30 march

---
+++ fynd/vuejs/helloworld/src/components/Test30Marmath.js
@@ -0,0 +1,42 @@
+
+var math=require('mathematics');
+
+// addition
+function add(num1, num2) {
+    return num1 * num2;
+  }
+  
+  const result1 = add(2, 3);
+  
+  console.log(`Result: ${result1}`);
+
+  // substraction
+
+  function sub(num1, num2) {
+    return num1 * num2;
+  }
+  
+  const result2 = sub(6, 4);
+  
+  console.log(`Result: ${result2}`);
+
+  // multiplication
+
+  function mul(num1, num2) {
+    return num1 * num2;
+  }
+  
+  const result3 = mul(5, 8);
+  
+  console.log(`Result: ${result3}`);
+
+  // division
+
+  function div(num1, num2) {
+    return num1 * num2;
+  }
+  
+  const result4 = div(3, 9);
+  
+  console.log(`Result: ${result4}`);
+  
